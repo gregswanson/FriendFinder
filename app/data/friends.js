77709733c4cc8000d924586abd7eda801b052db2
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-04-29 01:21:01 -0400

    added server logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,20 +3,52 @@ var friends = [
   "name":"Greg",
   "photo":"http://stormy-everglades-82892.herokuapp.com/img/profileimage.jpg",
   "scores":[
-     5,
      1,
-     4,
-     4,
-     5,
      1,
-     2,
-     5,
-     4,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
      1
+     ]
+},
+{
+  "name":"Greg2",
+  "photo":"http://stormy-everglades-82892.herokuapp.com/img/profileimage.jpg",
+  "scores":[
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2
+     ]
+},
+{
+  "name":"Greg3",
+  "photo":"http://stormy-everglades-82892.herokuapp.com/img/profileimage.jpg",
+  "scores":[
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3
      ]
 }
 
-]
+];
 
 
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,9 +11,31 @@ module.exports = function(app){
 	
 
 	app.post('/API/friends', function(req,res){
-	console.log(req.body);
+	var newscore = req.body.scores;
+	var newscoreint = 0;
+	var currentscoreint = [];
+	
+	
+	for (var i = 0; i < newscore.length; i++) {
+		newscoreint += parseInt(newscore[i]);
+	}
+
+	for (var j = 0; j < friends.length; j++){
+		currentscoreint.push(friends[j].scores.reduce(function(a, b) { return a + b; }, 0));		
+	}
+
+	for (var k = 0; k < currentscoreint.length; k++) {
+		if (currentscoreint[k] == newscoreint) {
+			res.send(friends[k]);
+		}
+	}
+	
+
+	//console.log(currentscoreint);
+	
+	
+	
 	friends.push(req.body);
-	res.send('friends');
 	});
 
 
